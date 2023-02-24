a997e0c13e41d7107349936d622a340914ec71b9
sheflywithwings <aceinfotech20@gmail.com>
2023-02-23 16:05:25 -0800

    use `floatStrTrim` in all case

--- src/utils/dimension.jsx
+++ src/utils/dimension.jsx
@@ -1,15 +1,9 @@
 import { assertDefined } from './assert.jsx'
+import { floatStrTrim } from './common.jsx'
 import { Config } from './config.jsx'
 import { DIM_INCH, DIM_FEET, DIM_METER, DIM_CENTIMETER, DIM_MILLIMETER } from './constants'
 
-const decimals = 1000
-
 export class Dimension {
-  static round(value) {
-    assertDefined(value)
-    return Math.round(decimals * value) / decimals
-  }
-
   /**
    * Convert dimension number to real number based on centimeter unit
    *
@@ -25,19 +19,19 @@ export class Dimension {
 
     switch (unit) {
       case DIM_FEET:
-        real = Dimension.round(measure * 30.480016459203095991 * scale)
+        real = floatStrTrim(measure * 30.480016459203095991 * scale)
         break
       case DIM_INCH:
-        real = Dimension.round(measure * 2.5400013716002578512 * scale)
+        real = floatStrTrim(measure * 2.5400013716002578512 * scale)
         break
       case DIM_MILLIMETER:
-        real = Dimension.round(measure * 0.10000005400001014955 * scale)
+        real = floatStrTrim(measure * 0.10000005400001014955 * scale)
         break
       case DIM_CENTIMETER:
-        real = Dimension.round(measure * scale)
+        real = floatStrTrim(measure * scale)
         break
       case DIM_METER:
-        real = Dimension.round(measure * 100 * scale)
+        real = floatStrTrim(measure * 100 * scale)
         break
       default:
         real = measure
@@ -62,19 +56,19 @@ export class Dimension {
 
     switch (unit) {
       case DIM_FEET:
-        dimension = Dimension.round((real * 0.032808416666669996953) / scale)
+        dimension = floatStrTrim((real * 0.032808416666669996953) / scale)
         break
       case DIM_INCH:
-        dimension = Dimension.round((real * 0.3937) / scale)
+        dimension = floatStrTrim((real * 0.3937) / scale)
         break
       case DIM_MILLIMETER:
-        dimension = Dimension.round((real * 10) / scale)
+        dimension = floatStrTrim((real * 10) / scale)
         break
       case DIM_CENTIMETER:
-        dimension = Dimension.round(real / scale)
+        dimension = floatStrTrim(real / scale)
         break
       case DIM_METER:
-        dimension = Dimension.round((real * 0.01) / scale)
+        dimension = floatStrTrim((real * 0.01) / scale)
         break
       default:
         dimension = real
@@ -93,7 +87,7 @@ export class Dimension {
   static pixelToReal(pixel) {
     const scale = Config.getNumericValue('scale')
     assertDefined(pixel, scale)
-    return Dimension.round(pixel * scale)
+    return floatStrTrim(pixel * scale)
   }
 
   /**
@@ -105,6 +99,6 @@ export class Dimension {
   static realToPixel(real) {
     const scale = Config.getNumericValue('scale')
     assertDefined(real, scale)
-    return Dimension.round(real / scale)
+    return floatStrTrim(real / scale)
   }
 }
